8713894fcb9b23bec7243838a1d74e0af54198d4
rkolesnikov <roman_kolesnikov@govitall.com>
2023-10-02 17:41:42 +0300

    feat: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import {Counter} from "./components/Counter";
+import React, {lazy, Suspense} from "react";
+import {NavLink, Route, Routes} from "react-router-dom";
+
+const About = lazy(() => import('./pages/About'));
+const Home = lazy(() => import('./pages/Home'));
 
 const App = () => {
     return (
         <div className="app">
-            <Counter/>
+            <nav>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink  to='/about'>About</NavLink>
+            </nav>
+            <Suspense fallback={'Loading...'}>
+                <Routes>
+                    <Route path='/about' element={<About/>}/>
+                    <Route path='/' element={<Home/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,11 @@
 import {useState} from "react";
 import styles from "./Counter.module.scss";
 
-console.log(styles)
 export const Counter = () => {
     const [count, setCount] = useState(0);
 
     return (
         <div className={styles.counter}>
-            <h1>Hello CodeSandbox</h1>
             <h2>You clicked {count} times!</h2>
 
             <button onClick={() => setCount(count - 1)}>Decrement</button>
