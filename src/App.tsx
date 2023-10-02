c6e19e782cd81c8e662fd69dd41564fc1cbf33e1
rkolesnikov <roman_kolesnikov@govitall.com>
2023-10-02 19:03:56 +0300

    feat: add hepler classNames

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {lazy, Suspense} from "react";
 import {NavLink, Route, Routes} from "react-router-dom";
 import useTheme from "./theme/useTheme";
 import ThemeToggle from "./components/ThemeToggle";
+import {classNames} from "./helpers/classNames/classNames";
 
 const About = lazy(() => import('./pages/About'));
 const Home = lazy(() => import('./pages/Home'));
@@ -9,7 +10,7 @@ const Home = lazy(() => import('./pages/Home'));
 const App = () => {
     const {theme, toggleTheme} = useTheme()
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <div className="d-flex">
                 <nav>
                     <NavLink to='/'>Home</NavLink>
