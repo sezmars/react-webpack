7536b2de3030b92f7237011d314b1d109a377cc6
rkolesnikov <roman_kolesnikov@govitall.com>
2023-10-02 20:11:44 +0300

    Updates

--- 802cb889cb5aa94f3eed9ff.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebpackGhPages=self.webpackChunkwebpackGhPages||[]).push([[802],{802:(e,a,s)=>{s.r(a),s.d(a,{default:()=>n});var c=s(893);const n=function(){return(0,c.jsx)("div",{className:"nvjwlXbL",children:(0,c.jsx)("b",{children:"Smart Webpack setup for React"})})}}}]);
