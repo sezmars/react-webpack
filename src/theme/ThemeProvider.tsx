b03a38dad82c67e52073fd5147e2c4e0667926ce
rkolesnikov <roman_kolesnikov@govitall.com>
2023-10-02 18:55:16 +0300

    feat: add theme

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,20 @@
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
+import {ReactNode, useMemo, useState} from "react";
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
+export const ThemeProvider = ({children}: { children: ReactNode }) => {
+    const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+    const defaultProps = useMemo(() => (
+        {
+            theme,
+            setTheme
+        }
+    ), [theme]);
+
+    return (
+        <ThemeContext.Provider value={defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
