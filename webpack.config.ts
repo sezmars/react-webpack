22901cb350753d822166ed1c47bf10120afd9372
rkolesnikov <roman_kolesnikov@govitall.com>
2023-10-02 16:04:56 +0300

    feat: add dev server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,20 @@
 import webpack from 'webpack';
 
 import buildWebpackConfig from "./config/build/buildWebpackConfig";
-import {BuildPaths} from "./config/build/types/config";
-import path from "path"; //to access built-in plugins
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
+import path from "path"; // to access built-in plugins
 
-const buildPaths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    build: path.resolve(__dirname, 'build'),
-    html: path.resolve(__dirname, 'public', 'index.html'),
-}
 
-const config: webpack.Configuration = buildWebpackConfig({mode: "development", paths: buildPaths })
-export default config;
+export default (env: BuildEnv) => {
+    const buildPaths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
+
+    const mode = env.mode || "development"
+    const isDev = env.mode === "development"
+    const PORT: number = env.port || 3000;
+
+    return buildWebpackConfig({mode, paths: buildPaths, port: PORT, isDev})
+};
