b8e3ba3652588e24a22f63be38b70d663edc6478
rkolesnikov <roman_kolesnikov@govitall.com>
2023-10-02 17:05:21 +0300

    feat: add MiniCssExtractPlugin & style isolation

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import {Counter} from "./components/Counter";
 
 const App = () => {
     return (
-        <>
+        <div className="app">
             <Counter/>
-        </>
+        </div>
     );
 };
 
