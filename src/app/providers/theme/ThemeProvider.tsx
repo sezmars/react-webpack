7ba000de666b6f62d67f2d11247189e92894ac97
rkolesnikov <roman_kolesnikov@govitall.com>
2023-10-03 15:25:38 +0300

    fix: return

--- src/app/providers/theme/ThemeProvider.tsx
+++ src/app/providers/theme/ThemeProvider.tsx
@@ -2,7 +2,7 @@ import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
 import {ReactNode, useMemo, useState} from "react";
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
-export const ThemeProvider = ({children}: { children: Record }) => {
+export const ThemeProvider = ({children}: { children: ReactNode }) => {
     const [theme, setTheme] = useState<Theme>(defaultTheme);
 
     const defaultProps = useMemo(() => (
